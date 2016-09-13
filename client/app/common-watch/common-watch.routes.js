9df644ed7c4f71347ada0283c66b675c25e55736
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2016-09-14 01:37:40 +0300

    Added id param to router on client

--- client/app/common-watch/common-watch.routes.js
+++ client/app/common-watch/common-watch.routes.js
@@ -4,7 +4,7 @@ export default function($stateProvider) {
   'ngInject';
   $stateProvider
     .state('common-watch', {
-      url: '/common-watch',
+      url: '/common-watch/:id',
       template: '<common-watch></common-watch>'
     });
 }
