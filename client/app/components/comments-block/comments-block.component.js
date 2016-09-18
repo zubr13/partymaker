fe5e31a2352aa7286532d03d0db9d4bc590d3fcf
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2016-09-18 21:02:48 +0300

    Added comments of video in session

--- client/app/components/comments-block/comments-block.component.js
+++ client/app/components/comments-block/comments-block.component.js
@@ -14,6 +14,10 @@ export class commentsBlockComponent {
     this.comments.push({message: this.comment, author: this.user.name || 'Anon'});
     this.videoService.addComment(this.video,  {message: this.comment, author: this.user.name || 'Anon'});
   }
+
+  addSessionComment(){
+    this.videoService.addSessionComment(this.session,  {message: this.comment, author: this.user.name || 'Anon'});
+  }
 }
 
 commentsBlockComponent.$inject = ['videoService', '$scope'];
@@ -21,7 +25,7 @@ commentsBlockComponent.$inject = ['videoService', '$scope'];
 export default angular.module('partymakerApp.comments-block', [])
   .component('commentsBlock', {
     template: require('./comments-block.component.html'),
-    bindings: { comments: '<', video: '<', user: '<'},
+    bindings: { comments: '<', video: '<', user: '<', session: '<'},
     controller: commentsBlockComponent
   })
   .name;
