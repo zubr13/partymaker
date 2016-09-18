10b7cafb82be46fb287d8beefdb66cf2917c369a
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2016-09-18 15:47:19 +0300

    Made user binding with chat and comments

--- client/app/components/chat/chat.component.js
+++ client/app/components/chat/chat.component.js
@@ -17,17 +17,17 @@ export class chatComponent {
   }
 
   sendMessage() {
-    this.socket.emit('chat message', {date: new Date(), message: this.messageValue, author: "Petya"});
+    this.socket.emit('chat message', {date: new Date(), message: this.messageValue, author: this.user.name});
     const message = this.messageValue;
     this.messageValue = '';
-    this.videoService.saveMessage(this.session, {message: message, author: "Petya"});
+    this.videoService.saveMessage(this.session, {message: message, author: this.user.name});
   }
 }
 
 export default angular.module('partymakerApp.chat', [])
   .component('chat', {
     template: require('./chat.component.html'),
-    bindings: { messages: '<', session: '<'},
+    bindings: { messages: '<', session: '<', user: '<'},
     controller: chatComponent
   })
   .name;
